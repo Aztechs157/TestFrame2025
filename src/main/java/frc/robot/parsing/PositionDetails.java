39546b153200ea706cdd54981de3ef0d1c73c748
Luca Forever and Ever <silverluca76@gmail.com>
2025-01-28 20:40:53 -0500

    Started work on changing the position_details schema to provide system positions rather than simply the height of elements - LS

--- src/main/java/frc/robot/parsing/PositionDetails.java
+++ src/main/java/frc/robot/parsing/PositionDetails.java
@@ -6,9 +6,11 @@ package frc.robot.parsing;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Dictionary;
 import java.util.Hashtable;
 import java.util.Iterator;
+import java.util.List;
 
 import edu.wpi.first.wpilibj.Filesystem;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -17,8 +19,20 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 /** Add your docs here. */
 public class PositionDetails {
     private final String JSONPath = "/position_details.json";
-    private final double ARBITRARY_TAG_ID = 6;
+    public Stage[] corals;
     public Dictionary<Integer, ReefTag> reefTags = new Hashtable<>();
+
+    public class Stage {
+        public double verticalPos;
+        public double leftOffset;
+        public double rightOffset;
+
+        public Stage(JsonNode stageJSON) {
+            this.verticalPos = stageJSON.get("verticalPos").asDouble();
+            this.leftOffset = stageJSON.get("horizontalOffsets").get("left").asDouble();
+            this.rightOffset = stageJSON.get("horizontalOffsets").get("right").asDouble();
+        }
+    }
 
     public PositionDetails() {
         File file = new File(Filesystem.getDeployDirectory().toPath() + JSONPath);
@@ -30,6 +44,11 @@ public class PositionDetails {
                 int tagID = Integer.parseInt(currentTag.substring(3));
                 reefTags.put(tagID, new ReefTag(json.get("reef").get(currentTag), tagID));
             }
+            List<Stage> coralList = new ArrayList<Stage>(4);
+            for(int i = 1; i <= 4; i++) {
+                coralList.add(new Stage(json.get("reef").get("coral").get("stage" + i)));
+            }
+            coralList.toArray(corals);
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
@@ -45,28 +64,27 @@ public class PositionDetails {
         return reefTags.get(tagID).getAlgaeVerticalPos();
     }
     /**
-     * Get vertical position for the specified stage for the specified tag
+     * Get vertical position for the specified stage
      * @param stage - one-indexed ie: 1,2,3,4
      * @return a double representing the vertical position
      */
     public double getCoralVerticalPos(int stage) {
-        return reefTags.get(ARBITRARY_TAG_ID).getCoralVerticalPos(stage);
+        return corals[stage - 1].verticalPos;
     }
     /**
-     * Get left offset for the specified stage for the specified tag
+     * Get left offset for the specified stage
      * @param stage - one-indexed ie: 1,2,3,4
      * @return a double representing the left offset
      */
     public double getLeftOffset(int stage) {
-        return reefTags.get(ARBITRARY_TAG_ID).getLeftOffset(stage);
+        return corals[stage - 1].leftOffset;
     }
     /**
-     * Get right offset for the specified stage for the specified tag
+     * Get right offset for the specified stage
      * @param stage - one-indexed ie: 1,2,3,4
      * @return a double representing the right offset
      */
     public double getRightOffset(int stage) {
-        return reefTags.get(ARBITRARY_TAG_ID).getRightOffset(stage);
+        return corals[stage - 1].rightOffset;
     }
-
 }
--- src/main/java/frc/robot/parsing/ReefTag.java
+++ src/main/java/frc/robot/parsing/ReefTag.java
@@ -4,38 +4,18 @@
 
 package frc.robot.parsing;
 
-import java.util.ArrayList;
-import java.util.List;
 import com.fasterxml.jackson.databind.JsonNode;
 
 /** Add your docs here. */
 public class ReefTag {
     public int tagID;
     public String face;
-    public Stage[] corals;
     public double algaeVerticalPos;
-
-    public class Stage {
-        public double verticalPos;
-        public double leftOffset;
-        public double rightOffset;
-
-        public Stage(JsonNode stageJSON) {
-            this.verticalPos = stageJSON.get("verticalPos").asDouble();
-            this.leftOffset = stageJSON.get("horizontalOffsets").get("left").asDouble();
-            this.rightOffset = stageJSON.get("horizontalOffsets").get("right").asDouble();
-        }
-    }
 
     public ReefTag(JsonNode tagJSON, int tagID) {
         this.tagID = tagID; // TODO: Try to get tag ID from tagJSON
         this.face = tagJSON.get("face").asText();
         this.algaeVerticalPos = tagJSON.get("algaeVerticalPos").asDouble();
-        List<Stage> coralList = new ArrayList<Stage>(4);
-        for(int i = 1; i <= 4; i++) {
-            coralList.add(new Stage(tagJSON.get("coral").get("stage" + i)));
-        }
-        coralList.toArray(corals);
     }
 
     /**
@@ -44,29 +24,5 @@ public class ReefTag {
      */
     public double getAlgaeVerticalPos() {
         return algaeVerticalPos;
-    }
-    /**
-     * Get vertical position for the specified stage
-     * @param stage - one-indexed ie: 1,2,3,4
-     * @return a double representing the vertical position
-     */
-    public double getCoralVerticalPos(int stage) {
-        return corals[stage - 1].verticalPos;
-    }
-    /**
-     * Get left offset for the specified stage
-     * @param stage - one-indexed ie: 1,2,3,4
-     * @return a double representing the left offset
-     */
-    public double getLeftOffset(int stage) {
-        return corals[stage - 1].leftOffset;
-    }
-    /**
-     * Get right offset for the specified stage
-     * @param stage - one-indexed ie: 1,2,3,4
-     * @return a double representing the right offset
-     */
-    public double getRightOffset(int stage) {
-        return corals[stage - 1].rightOffset;
     }
 }
